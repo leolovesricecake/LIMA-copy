192746b8e9fa3696982b3445aa301b3944e13d02
leolovesricecake <1427677862@qq.com>
2026-07-06 16:20:48 +0800

    delete lima_origin and useless baselines

--- lima_origin/baseline_attribution/IG2/saliency/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Library to calculate saliency using TF1 and other ML frameworks."""
-from . import core
-from . import tf1
--- lima_origin/run.sh
+++
@@ -1,4 +0,0 @@
-python -m submodular_attribution.efficientv2-smdl_explanation_imagenet_clip_vitl_superpixel \
-    --lambda1 1. --lambda4 1. \
-    --device 1 \
-    --save-dir ./test
